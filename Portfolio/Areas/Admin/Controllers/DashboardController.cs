3160e413670fb096af9f46d1469a20939583148f
omid-develop <omidataee20041382>
2026-06-04 13:18:56 +0330

    first commit

---
+++ Portfolio/Areas/Admin/Controllers/DashboardController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Portfolio.Areas.Admin.Controllers
+{
+    [Area("/admin")]
+    public class DashboardController : Controller
+    {
+
+        [Route("/admin/dashboard")]
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+
+    }
+}
